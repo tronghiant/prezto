775af0d491cb516e307d4a4af4c033c7f8286591
tronghiant(dan) <tronghiant@users.noreply.github.com>
2023-01-03 16:01:48 +0700

    custom configs at vnth-k0127 macbook

--- init.zsh
+++ init.zsh
@@ -168,8 +168,8 @@ function pmodload {
 ZPREZTODIR=${0:h}
 
 # Source the Prezto configuration file.
-if [[ -s "${ZDOTDIR:-$HOME}/.zpreztorc" ]]; then
-  source "${ZDOTDIR:-$HOME}/.zpreztorc"
+if [[ -s "$ZPREZTODIR/runcoms/zpreztorc" ]]; then
+  source "$ZPREZTODIR/runcoms/zpreztorc"
 fi
 
 # Disable color and theme in dumb terminals.
